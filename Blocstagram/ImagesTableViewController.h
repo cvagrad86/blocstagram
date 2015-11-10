c1635cf07d744ae9c7ea137b4db1f8fccaf05135
Eric Chamberlin <echamberlin@me.com>
2015-11-10 11:33:16 +0100

    first attempt to run app, no pics show up.

---
+++ Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  ImagesTableViewController.h
+//  
+//
+//  Created by Eric Chamberlin on 11/10/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
