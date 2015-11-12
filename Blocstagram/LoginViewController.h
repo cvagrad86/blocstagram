8e8cc8766e6dd95c7e2b4ce2377a4509ecc19c2a
Eric Chamberlin <echamberlin@me.com>
2015-11-12 10:37:24 +0100

    moving on

---
+++ Blocstagram/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  Blocstagram
+//
+//  Created by Eric Chamberlin on 11/12/15.
+//  Copyright © 2015 Eric Chamberlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+extern NSString *const LoginViewControllerDidGetAccessTokenNotification;
+
+@end
--- DataSource.h
+++ DataSource.h
@@ -13,16 +13,20 @@
 typedef void (^NewItemCompletionBlock)(NSError *error);
 
 
+
 @interface DataSource : NSObject
 
 +(instancetype) sharedInstance;
 
 @property (nonatomic, strong, readonly) NSArray *mediaItems;
+@property (nonatomic, strong, readonly) NSString *accessToken;
 
 -(void) deleteMediaItem:(Media *) item;
 
 - (void) requestNewItemsWithCompletionHandler:(NewItemCompletionBlock)completionHandler;
 - (void) requestOldItemsWithCompletionHandler:(NewItemCompletionBlock)completionHandler;
+
++ (NSString *) instagramClientID;
 
 
 
