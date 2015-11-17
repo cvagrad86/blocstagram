ddddb3f041d49db57c8c196e04f2d8ee37eea699
Eric Chamberlin <echamberlin@me.com>
2015-11-17 11:25:46 +0100

    see message I sent regarding question on assignment.

---
+++ Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,16 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Eric Chamberlin on 11/17/15.
+//  Copyright © 2015 Eric Chamberlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+
+@end
